5df787f948f41e255dfe91a07b2a9d4e4224c171
mareylife <marey0204@gmail.com>
2023-03-11 19:58:31 +0330

    TVShow functions

--- src/main/java/org/example/TVShow.java
+++ src/main/java/org/example/TVShow.java
@@ -3,8 +3,50 @@ package org.example;
 import java.util.ArrayList;
 
 class TVShow {
+    private String title;
+    private String genre;
+    private int releaseYear;
+    private double imdbScore;
+    private double duration;
+
+    private ArrayList<String> cast;
+
+    public TVShow(String title,String genre,int releaseYear,double imdbScore,double duration, ArrayList<String> cast){
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.imdbScore = imdbScore;
+        this.duration = duration;
+        this.cast = cast;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public double getImdbScore() {
+        return imdbScore;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public ArrayList<String> getCast() {
+        return cast;
+    }
+
+
     /*
-    *The TVShow should have a title , genre, release year, duration and a rating.
+     *The TVShow should have a title , genre, release year, duration and a rating.
      *The TVShow should have an ArrayList of the cast.
      */
 
