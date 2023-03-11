08db0c29e5ee24d57f000c71e00675dcae09aad1
mareylife <marey0204@gmail.com>
2023-03-11 19:56:48 +0330

    NetflixService

--- src/main/java/org/example/NetflixService.java
+++ src/main/java/org/example/NetflixService.java
@@ -1,27 +1,64 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 class NetflixService {
+    User currentUser;
+    public ArrayList<User> users;
+    public ArrayList<TVShow> tvShows;
+    public ArrayList<Movie> movies;
+    public NetflixService()
+    {
+    }
     /*
      *The NetflixService should have an Arraylist of users, tv shows and movies.
      *The NetflixService should have a User object which represents current user.
      */
 
     public void addTVShow(TVShow tvShow){
-        // Implement add tv show logic here
+        tvShows.add(tvShow);
     }
 
     public void addMovie(Movie movie){
-        // Implement add movie logic here
+        movies.add(movie);
     }
 
     public void createAccount(String username, String password) {
-        // Implement create account logic here
+        if(users.contains(currentUser))
+        {
+            System.err.println("this username exist. please try again");
+        }
+        else {
+            currentUser = new User(username,password);
+            users.add(currentUser);
+        }
     }
 
     public boolean login(String username, String password) {
-        // Implement login logic here
+        if(users.contains(currentUser))
+        {
+            for(int i = 0; i < users.size(); i++)
+            {
+                if(username.equals(users.get(i).getUsername()))
+                {
+                    if(password.equals(users.get(i).getPassword()))
+                    {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        else {
+            System.err.println("this username doesn't exist");
+            return false;
+        }
         return false;
     }
 
@@ -30,18 +67,39 @@ class NetflixService {
     }
 
     public ArrayList<TVShow> searchByTitle(String title) {
-        // Implement search by title logic here
-        return null;
+        ArrayList<TVShow> searchResult = new ArrayList<>();
+
+        for (int i = 0; i < tvShows.size(); i++) {
+            if(tvShows.get(i).getTitle().contains(title))
+            {
+                searchResult.add(tvShows.get(i));
+            }
+        }
+        return searchResult;
     }
 
     public ArrayList<TVShow> searchByGenre(String genre) {
-        // Implement search by genre logic here
-        return null;
+        ArrayList<TVShow> searchResult = new ArrayList<>();
+
+        for (int i = 0; i < tvShows.size(); i++) {
+            if(tvShows.get(i).getGenre().equals(genre))
+            {
+                searchResult.add(tvShows.get(i));
+            }
+        }
+        return searchResult;
     }
 
     public ArrayList<TVShow> searchByReleaseYear(int year) {
-        // Implement search by release year logic here
-        return null;
+        ArrayList<TVShow> searchResult = new ArrayList<>();
+
+        for (int i = 0; i < tvShows.size(); i++) {
+            if(tvShows.get(i).getReleaseYear() == year)
+            {
+                searchResult.add(tvShows.get(i));
+            }
+        }
+        return searchResult;
     }
 
 
